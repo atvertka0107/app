d91e2cb7f254e3266ec2987626854603108ecee5
Egor <egor-syrov@bk.ru>
2022-01-06 17:12:10 +0300

    second practika

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 "use strict";
+function getAnswer(str2) {
+    let temp = '';
+    while (temp == "" || temp == null) {
+        temp = prompt(str2, "");
+    }
+    if (temp.length > 50) {
+        getAnswer(str2);
+    }
+    return temp;
+}
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let mostLoveFilm = getAnswer("Название любимого фильма?");
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: {},
+    genres: [],
     privat: false
 };
 
-let lastFilm = prompt("Последний просмотренный фильм?", "Гарри Потер и принц полукровка");
-let markOfFilm = +prompt("Оцените его от 1 до 10","");
+if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("вы киноман");
+} else {
+    console.log("error");
+}
 
-personalMovieDB.movies[lastFilm] = markOfFilm;
+for (let i = 0; i < 2; i++) {
+    let a = getAnswer("Один из последних просмотренных фильмов"),
+        b = +prompt("оцените от 0 до 10", "");
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
+
+alert(`me too, like ${mostLoveFilm}`);
